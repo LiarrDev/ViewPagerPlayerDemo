8a63ccf45adab01e987c09c9f49c9c5b8f80097c
Liarr <liarr@foxmail.com>
2022-03-31 17:24:04 +0800

    Feat: Screenshot

--- app/src/main/java/io/liarr/viewpagerplayerdemo/Videos.kt
+++ app/src/main/java/io/liarr/viewpagerplayerdemo/Videos.kt
@@ -1,13 +1,13 @@
 package io.liarr.viewpagerplayerdemo
 
 val videoUrls = listOf(
+    "https://v26-web.douyinvod.com/f1993b39bf8aea916455b12b9ec8aaa4/624577e9/video/tos/cn/tos-cn-ve-15c001-alinc2/78604c09c9734b508bed3cdacb8ca9c8/",
+    "https://v26-web.douyinvod.com/79ff49e70cbb7a1d9836bac1482b1168/62457757/video/tos/cn/tos-cn-ve-15c001-alinc2/a2e0800c0e0f472aa1cb9dc8090c7ef3/",
+    "https://v26-web.douyinvod.com/8613bf9d0a0d0edc2b6840a490f8d978/624577ed/video/tos/cn/tos-cn-ve-15c001-alinc2/d1b0a726f6a0487587537b2d9a4e2653/",
+    "https://v26-web.douyinvod.com/62ca1a071f61b1fc8ac8c7ab8c44853a/62457746/video/tos/cn/tos-cn-ve-15c001-alinc2/dafd91e5c67840fb933e9c1c4f6f2cb5/",
     "https://v26-web.douyinvod.com/5effb826c37e2521d3186819084c70ae/62457750/video/tos/cn/tos-cn-ve-15c001-alinc2/18394f9f9a064853bbbde8b130431d9d/",
     "https://v26-web.douyinvod.com/a8f2fa7d1bad9b5c8993a6e3ab198dde/6245780f/video/tos/cn/tos-cn-ve-15c001-alinc2/2beac31370f84766b0f95b1936003c64/",
     "https://v26-web.douyinvod.com/48780608f8af1af10eaa3cc14940046f/62457757/video/tos/cn/tos-cn-ve-15c001-alinc2/5c37af01145e488e99022552609c76d4/",
-    "https://v26-web.douyinvod.com/8613bf9d0a0d0edc2b6840a490f8d978/624577ed/video/tos/cn/tos-cn-ve-15c001-alinc2/d1b0a726f6a0487587537b2d9a4e2653/",
-    "https://v26-web.douyinvod.com/62ca1a071f61b1fc8ac8c7ab8c44853a/62457746/video/tos/cn/tos-cn-ve-15c001-alinc2/dafd91e5c67840fb933e9c1c4f6f2cb5/",
-    "https://v26-web.douyinvod.com/f1993b39bf8aea916455b12b9ec8aaa4/624577e9/video/tos/cn/tos-cn-ve-15c001-alinc2/78604c09c9734b508bed3cdacb8ca9c8/",
-    "https://v26-web.douyinvod.com/79ff49e70cbb7a1d9836bac1482b1168/62457757/video/tos/cn/tos-cn-ve-15c001-alinc2/a2e0800c0e0f472aa1cb9dc8090c7ef3/",
     "https://v26-web.douyinvod.com/aacde0ff090be4386b5c24a056b8d98b/6245775c/video/tos/cn/tos-cn-ve-15c001-alinc2/25cca32e279641e787677df9304f2463/",
     "https://v26-web.douyinvod.com/eb5ae16a3b46b2d609d15dedaf7d789e/62457788/video/tos/cn/tos-cn-ve-15c001-alinc2/e4478028659f45839ba85de1bb615c9e/",
     "https://v26-web.douyinvod.com/dd629dace801ad95e63ea410d7e5f416/62457763/video/tos/cn/tos-cn-ve-15c001-alinc2/8f1b5874dac2431a9c66f6c10fc30ad7/",
